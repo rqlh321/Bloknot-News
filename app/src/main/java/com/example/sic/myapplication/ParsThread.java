0b0876351eeb72f667fe2e72ed1ba3c3ca2c543c
rqlh321 <rqlh321@gmail.com>
2016-09-10 00:51:30 +0300

    background

--- app/src/main/java/com/example/sic/myapplication/ParsThread.java
+++ app/src/main/java/com/example/sic/myapplication/ParsThread.java
@@ -30,7 +30,7 @@ class ParsThread extends AsyncTask<Integer, Void, ArrayList<NewsItem>> {
                 String title = content.attributes().get("title");
                 String url = content.attributes().get("src");
                 if (url.contains("//s0.")) {//локальная
-                    url=url.replace("//s0.", "http://");
+                    url = url.replace("//s0.", "http://");
                     if (!adapter.contains(title)) {//уникальная
                         NewsItem item = new NewsItem(title, url);
                         parsNewsList.add(item);
