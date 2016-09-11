dc52beac35742421cb30bbd639dbe758c632f334
rqlh321 <rqlh321@gmail.com>
2016-09-12 00:26:40 +0300

    delete asyncTask add rxJava

--- app/src/main/java/com/example/sic/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/sic/myapplication/Activity/MainActivity.java
@@ -9,11 +9,22 @@ import android.widget.ProgressBar;
 
 import com.example.sic.myapplication.EndlessRecyclerOnScrollListener;
 import com.example.sic.myapplication.NewsItem;
-import com.example.sic.myapplication.ParsThread;
 import com.example.sic.myapplication.R;
 import com.example.sic.myapplication.RecycleViewListAdapter;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
 import java.util.ArrayList;
+
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
     private final String SAVED_ITEMS = "saved";
@@ -28,7 +39,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        progressBar= (ProgressBar) findViewById(R.id.loading);
+        progressBar = (ProgressBar) findViewById(R.id.loading);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.news_list);
         recyclerView.setHasFixedSize(true);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -38,18 +49,20 @@ public class MainActivity extends Activity {
         recyclerView.addOnScrollListener(new EndlessRecyclerOnScrollListener(layoutManager) {
             @Override
             public void onLoadMore() {
-                new ParsThread(adapter,progressBar).execute(currentPage);
+                getNews(currentPage);
                 currentPage++;
                 progressBar.setVisibility(View.VISIBLE);
             }
         });
         if (savedInstanceState != null) {
+            progressBar.setVisibility(View.GONE);
             Bundle bundle = savedInstanceState.getBundle(SAVED_ITEMS);
             currentPage = bundle.getInt(CURRENT_PAGE);
             ArrayList<NewsItem> news = bundle.getParcelableArrayList(NEWS);
             adapter.addAll(news);
         } else {
-            new ParsThread(adapter,progressBar).execute(1);
+            currentPage = 1;
+            getNews(currentPage);
             currentPage++;
         }
     }
@@ -63,4 +76,50 @@ public class MainActivity extends Activity {
         super.onSaveInstanceState(outState);
     }
 
+    private void getNews(int page) {
+        Observable.just(page)
+                .map(new Func1<Integer, ArrayList<NewsItem>>() {
+                    @Override
+                    public ArrayList<NewsItem> call(Integer integer) {
+                        ArrayList<NewsItem> parsNewsList = new ArrayList<>();
+                        try {
+                            Document doc = Jsoup.connect("http://bloknot-taganrog.ru/?PAGEN_1=" + integer).get();
+                            Elements contentPictures = doc.select(".preview_picture");
+                            for (Element content : contentPictures) {
+                                String title = content.attributes().get("title");
+                                String url = content.attributes().get("src");
+                                if (url.contains("//s0.")) {//локальная новость
+                                    url = url.replace("//s0.", "http://");
+                                    if (!adapter.contains(title)) {//должна быть уникальной
+                                        NewsItem item = new NewsItem(title, url);
+                                        parsNewsList.add(item);
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return parsNewsList;
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ArrayList<NewsItem>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(ArrayList<NewsItem> newsItems) {
+                        adapter.addAll(newsItems);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
 }
