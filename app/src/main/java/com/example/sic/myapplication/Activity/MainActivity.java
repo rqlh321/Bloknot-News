1abc263139b594f8cb6679ef5470ee6462c20b7e
rqlh321 <rqlh321@gmail.com>
2016-09-14 13:45:49 +0300

    receiver setup

--- app/src/main/java/com/example/sic/myapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/sic/myapplication/Activity/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.sic.myapplication.Activity;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.Snackbar;
@@ -9,10 +12,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.sic.myapplication.EndlessRecyclerOnScrollListener;
+import com.example.sic.myapplication.NetworkChangeReceiver;
 import com.example.sic.myapplication.NewsItem;
 import com.example.sic.myapplication.R;
 import com.example.sic.myapplication.RecycleViewListAdapter;
@@ -33,20 +36,70 @@ import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 public class MainActivity extends Activity {
+    private static int currentPage = 1;
+    private static RecycleViewListAdapter adapter;
+    private static ProgressBar progressBar;
     private final String SAVED_ITEMS = "saved";
     private final String NEWS = "news";
     private final String CURRENT_PAGE = "current_page";
     private ServiceManager serviceManager;
     private CollapsingToolbarLayout mainActivityView;
-    private static int currentPage = 1;
-    private static RecycleViewListAdapter adapter;
-    private static ProgressBar progressBar;
+    private NetworkChangeReceiver networkChangeReceiver = new NetworkChangeReceiver();
+
+    public static void getNews() {
+        progressBar.setVisibility(View.VISIBLE);
+        Observable.just(currentPage)
+                .map(new Func1<Integer, ArrayList<NewsItem>>() {
+                    @Override
+                    public ArrayList<NewsItem> call(Integer integer) {
+                        ArrayList<NewsItem> parsNewsList = new ArrayList<>();
+                        try {
+                            Document doc = Jsoup.connect("http://bloknot-taganrog.ru/?PAGEN_1=" + integer).get();
+                            Elements contentPictures = doc.select(".preview_picture");
+                            for (Element content : contentPictures) {
+                                String title = content.attributes().get("title");
+                                String url = content.attributes().get("src");
+                                if (url.contains("//s0.")) {//локальная новость
+                                    url = url.replace("//s0.", "http://");
+                                    if (!adapter.contains(title)) {//должна быть уникальной
+                                        NewsItem item = new NewsItem(title, url);
+                                        parsNewsList.add(item);
+                                    }
+                                }
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        return parsNewsList;
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ArrayList<NewsItem>>() {
+                    @Override
+                    public void onCompleted() {
+
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+
+                    }
+
+                    @Override
+                    public void onNext(ArrayList<NewsItem> newsItems) {
+                        adapter.addAll(newsItems);
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        registerReceiver(networkChangeReceiver,new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
         progressBar = (ProgressBar) findViewById(R.id.loading);
         mainActivityView = (CollapsingToolbarLayout) findViewById(R.id.main_activity_view);
         serviceManager = new ServiceManager(this);
@@ -96,59 +149,17 @@ public class MainActivity extends Activity {
         }
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
     private void connectionLost() {
         Snackbar snackbar = Snackbar.make(mainActivityView, "No internet connection!", Snackbar.LENGTH_LONG);
         View sbView = snackbar.getView();
         TextView textView = (TextView) sbView.findViewById(android.support.design.R.id.snackbar_text);
         textView.setTextColor(Color.YELLOW);
         snackbar.show();
-    }
-
-    public static void getNews() {
-        progressBar.setVisibility(View.VISIBLE);
-        Observable.just(currentPage)
-                .map(new Func1<Integer, ArrayList<NewsItem>>() {
-                    @Override
-                    public ArrayList<NewsItem> call(Integer integer) {
-                        ArrayList<NewsItem> parsNewsList = new ArrayList<>();
-                        try {
-                            Document doc = Jsoup.connect("http://bloknot-taganrog.ru/?PAGEN_1=" + integer).get();
-                            Elements contentPictures = doc.select(".preview_picture");
-                            for (Element content : contentPictures) {
-                                String title = content.attributes().get("title");
-                                String url = content.attributes().get("src");
-                                if (url.contains("//s0.")) {//локальная новость
-                                    url = url.replace("//s0.", "http://");
-                                    if (!adapter.contains(title)) {//должна быть уникальной
-                                        NewsItem item = new NewsItem(title, url);
-                                        parsNewsList.add(item);
-                                    }
-                                }
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        return parsNewsList;
-                    }
-                })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<ArrayList<NewsItem>>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-
-                    }
-
-                    @Override
-                    public void onNext(ArrayList<NewsItem> newsItems) {
-                        adapter.addAll(newsItems);
-                        progressBar.setVisibility(View.GONE);
-                    }
-                });
     }
 }
